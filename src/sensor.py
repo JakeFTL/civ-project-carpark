6e2ac89030c80520a60210c3c4f166c8e7e09249
Jake <176844671+LgndryJake@users.noreply.github.com>
2024-11-05 22:52:57 +0800

    feat: add constructors and attributes for the display and sensor classes

--- src/sensor.py
+++ src/sensor.py
@@ -1,3 +1,20 @@
 class Sensor:
-    pass
-# Add constructors and attributes to the classes
+    def __init__(self,
+                 id,
+                 is_active,
+                 car_park,
+                 ):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"{self.id} Status: {self.is_active}"
+
+
+class EntrySensor(Sensor):
+    ...
+
+
+class ExitSensor(Sensor):
+    ...
